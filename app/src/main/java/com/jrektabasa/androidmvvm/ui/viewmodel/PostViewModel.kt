cf6d880d05f6a116c836990b499e2fbca2f4cadb
Jayrek <jrektabasa@gmail.com>
2024-01-14 23:05:13 +0800

    update viewmodel using liveData

--- app/src/main/java/com/jrektabasa/androidmvvm/ui/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/ui/viewmodel/PostViewModel.kt
@@ -1,6 +1,8 @@
 package com.jrektabasa.androidmvvm.ui.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jrektabasa.androidmvvm.data.BlogInterface
@@ -11,6 +13,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PostViewModel @Inject constructor(private val client: BlogInterface) : ViewModel() {
+
+    private val _posts: MutableLiveData<List<Post>> = MutableLiveData()
+    val posts: LiveData<List<Post>> = _posts
+
     init {
         viewModelScope.launch {
             getPosts()
@@ -18,7 +24,12 @@ class PostViewModel @Inject constructor(private val client: BlogInterface) : Vie
     }
 
     suspend fun getPosts() {
-        val posts: List<Post> = client.getPosts()
-        Log.d("tara ai", "getPosts: $posts")
+        try {
+            val response = client.getPosts()
+            _posts.value = response
+        } catch (e: Exception) {
+            _posts.value = emptyList()
+        }
+
     }
 }
