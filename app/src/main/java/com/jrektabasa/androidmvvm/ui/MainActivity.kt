2e59079cee4311419579fc1842becb42afd29483
Jayrek <jrektabasa@gmail.com>
2024-01-14 23:05:39 +0800

    add viewBinding and adapter

--- app/src/main/java/com/jrektabasa/androidmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/ui/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.jrektabasa.androidmvvm.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jrektabasa.androidmvvm.R
 import com.jrektabasa.androidmvvm.ui.viewmodel.PostViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.jrektabasa.androidmvvm.databinding.ActivityMainBinding
+import com.jrektabasa.androidmvvm.model.Post
+import com.jrektabasa.androidmvvm.ui.adapter.BlogPostAdapter
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var blogPostAdapter: BlogPostAdapter
+
     private val viewModel: PostViewModel by viewModels()
+    private val blogPosts = mutableListOf<Post>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        displayPosts()
         getPosts()
     }
 
@@ -23,5 +33,16 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             viewModel.getPosts()
         }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun displayPosts() {
+        viewModel.posts.observe(this) { posts ->
+            blogPosts.clear()
+            blogPosts.addAll(posts)
+            blogPostAdapter.notifyDataSetChanged()
+        }
+        blogPostAdapter = BlogPostAdapter(this, blogPosts)
+        binding.recyclerView.adapter = blogPostAdapter
     }
 }
