440bd4d8e3ec7df9653ea47a72bda2dcafa1e4e5
Jayrek <jrektabasa@gmail.com>
2024-01-14 23:02:18 +0800

    update BlogPostAdapter.kt using view binding

--- app/src/main/java/com/jrektabasa/androidmvvm/ui/adapter/BlogPostAdapter.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/ui/adapter/BlogPostAdapter.kt
@@ -1,41 +1,29 @@
 package com.jrektabasa.androidmvvm.ui.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.jrektabasa.androidmvvm.R
+import com.jrektabasa.androidmvvm.databinding.BlogItemBinding
 import com.jrektabasa.androidmvvm.model.Post
 
 class BlogPostAdapter(
-    private val context: Context,
-    private val posts: List<Post>,
+    private val posts: MutableList<Post>,
 ) : RecyclerView.Adapter<BlogPostAdapter.ViewHolder>() {
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val tvId = itemView.findViewById<TextView>(R.id.textview_id)
-        private val tvTitle = itemView.findViewById<TextView>(R.id.textview_title)
-        private val tvBody = itemView.findViewById<TextView>(R.id.textview_body)
-
-        fun bind(post: Post) {
-            tvId.text = "Post: ${post.id}"
-            tvTitle.text = post.title
-            tvBody.text = post.body
-        }
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.blog_item, parent, false)
+        val view = BlogItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return posts.size
-    }
+    inner class ViewHolder(var itemBinding: BlogItemBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+
+    override fun getItemCount(): Int = posts.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(posts[position])
+        val post = posts[position]
+        holder.itemBinding.textviewId.text = "Post: ${post.id}"
+        holder.itemBinding.textviewTitle.text = post.title
+        holder.itemBinding.textviewBody.text = post.body
     }
 }
