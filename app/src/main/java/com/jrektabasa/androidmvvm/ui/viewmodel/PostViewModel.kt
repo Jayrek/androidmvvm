ec2d6a73800ff62f0f930b8ff2d632531cadaf54
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2024-01-14 17:21:34 +0800

    update PostViewModel.kt

--- app/src/main/java/com/jrektabasa/androidmvvm/ui/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/ui/viewmodel/PostViewModel.kt
@@ -3,19 +3,22 @@ package com.jrektabasa.androidmvvm.ui.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jrektabasa.androidmvvm.data.api.ApiClient
+import com.jrektabasa.androidmvvm.data.BlogInterface
 import com.jrektabasa.androidmvvm.model.Post
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PostViewModel : ViewModel() {
+@HiltViewModel
+class PostViewModel @Inject constructor(private val client: BlogInterface) : ViewModel() {
     init {
         viewModelScope.launch {
             getPosts()
         }
     }
 
-    private suspend fun getPosts() {
-        val posts: List<Post> = ApiClient.api.getPosts()
+    suspend fun getPosts() {
+        val posts: List<Post> = client.getPosts()
         Log.d("tara ai", "getPosts: $posts")
     }
 }
