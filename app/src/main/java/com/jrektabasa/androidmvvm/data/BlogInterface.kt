f079e30583ed30af6d69ca98e8eaf63d6b82640d
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2024-01-14 17:19:40 +0800

    moved ApiInterface.kt with BlogInterface.kt

--- app/src/main/java/com/jrektabasa/androidmvvm/data/BlogInterface.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/data/BlogInterface.kt
@@ -1,9 +1,9 @@
-package com.jrektabasa.androidmvvm.data.api
+package com.jrektabasa.androidmvvm.data
 
 import com.jrektabasa.androidmvvm.model.Post
 import retrofit2.http.GET
 
-interface ApiInterface {
+interface BlogInterface {
 
     @GET("posts")
     suspend fun getPosts(): List<Post>
