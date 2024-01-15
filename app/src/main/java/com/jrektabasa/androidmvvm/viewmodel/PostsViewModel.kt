e4957c0bf749c552e38fe7e21303e02a98727a36
Jayrek <jrektabasa@gmail.com>
2024-01-15 18:51:29 +0800

    rename PostViewModel.kt to PostsViewModel.kt

--- app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/PostsViewModel.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/PostsViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PostViewModel @Inject constructor(private val blogRepository: BlogRepository) : ViewModel() {
+class PostsViewModel @Inject constructor(private val blogRepository: BlogRepository) : ViewModel() {
 
     private val _posts: MutableLiveData<List<Post>> = MutableLiveData()
     val posts: LiveData<List<Post>> = _posts
