1ae95d0c9c8bc66289677765e9ec7167886af9d6
Jayrek <jrektabasa@gmail.com>
2024-01-15 18:50:46 +0800

    add getPostDetails, and getUserDetails method in repository

--- app/src/main/java/com/jrektabasa/androidmvvm/repository/BlogRepository.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/repository/BlogRepository.kt
@@ -1,8 +1,11 @@
 package com.jrektabasa.androidmvvm.repository
 
 import com.jrektabasa.androidmvvm.model.Post
+import com.jrektabasa.androidmvvm.model.User
 
 interface BlogRepository {
 
     suspend fun getPosts(): List<Post>
+    suspend fun getPostDetails(postId: Int): Post
+    suspend fun getUserDetails(userId: Int): User
 }
--- app/src/main/java/com/jrektabasa/androidmvvm/repository/impl/BlogRepositoryImpl.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/repository/impl/BlogRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.jrektabasa.androidmvvm.repository.impl
 
 import com.jrektabasa.androidmvvm.api.BlogInterface
 import com.jrektabasa.androidmvvm.model.Post
+import com.jrektabasa.androidmvvm.model.User
 import com.jrektabasa.androidmvvm.repository.BlogRepository
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,7 +11,17 @@ import javax.inject.Singleton
 class BlogRepositoryImpl @Inject constructor(
     private val blogInterface: BlogInterface
 ) : BlogRepository {
-    override suspend fun getPosts(): List<Post> {
-        return blogInterface.getPosts()
-    }
+    override suspend fun getPosts(): List<Post> =
+        blogInterface.getPosts()
+
+    override suspend fun getPostDetails(
+        postId: Int
+    ): Post =
+        blogInterface.getPostDetails(postId)
+
+    override suspend fun getUserDetails(
+        userId: Int
+    ): User =
+        blogInterface.getUserDetails(userId)
+
 }
