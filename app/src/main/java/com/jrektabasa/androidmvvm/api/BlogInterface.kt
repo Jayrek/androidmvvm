289d6ca84478f6e430bd622030ade9cd5c3fb372
Jayrek <jrektabasa@gmail.com>
2024-01-15 18:50:07 +0800

    add api endpoints (posts/{id}, users/{id}

--- app/src/main/java/com/jrektabasa/androidmvvm/api/BlogInterface.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/api/BlogInterface.kt
@@ -1,10 +1,18 @@
 package com.jrektabasa.androidmvvm.api
 
 import com.jrektabasa.androidmvvm.model.Post
+import com.jrektabasa.androidmvvm.model.User
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BlogInterface {
 
     @GET("posts")
     suspend fun getPosts(): List<Post>
+
+    @GET("posts/{id}")
+    suspend fun getPostDetails(@Path("id") postId: Int): Post
+
+    @GET("users/{id}")
+    suspend fun getUserDetails(@Path("id") userId: Int): User
 }
