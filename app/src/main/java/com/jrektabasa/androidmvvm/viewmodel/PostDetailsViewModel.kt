e6a366ea6a5d0dfc792e8d8543897a7c78956f84
Jayrek <jrektabasa@gmail.com>
2024-01-16 09:52:09 +0800

    chain request api

--- app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/PostDetailsViewModel.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/PostDetailsViewModel.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jrektabasa.androidmvvm.model.Post
 import com.jrektabasa.androidmvvm.model.User
 import com.jrektabasa.androidmvvm.repository.BlogRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,22 +17,29 @@ class PostDetailsViewModel @Inject constructor(
     private val blogRepository: BlogRepository
 ) : ViewModel() {
 
-    private val _postDetail: MutableLiveData<Post> = MutableLiveData()
-    val postDetail: LiveData<Post> = _postDetail
+    private val _isLoading: MutableLiveData<Boolean> = MutableLiveData()
+    val isLoading: LiveData<Boolean> = _isLoading
 
-    private val _userDetail: MutableLiveData<User> = MutableLiveData()
-    val userDetails: LiveData<User> = _userDetail
+    private val _postDetail: MutableLiveData<Post?> = MutableLiveData()
+    val postDetail: LiveData<Post?> = _postDetail
 
-    suspend fun getPostDetail(postId: Int) {
-        try {
-            val post = blogRepository.getPostDetails(postId)
-            val user = blogRepository.getUserDetails(post.userId)
-            _postDetail.value = post
-            _userDetail.value = user
-            Log.d("post", "getPostDetail: $post")
-            Log.d("user", "_userDetail: $user")
-        } catch (e: Exception) {
+    private val _userDetail: MutableLiveData<User?> = MutableLiveData()
+    val userDetails: LiveData<User?> = _userDetail
 
+    fun getPostDetail(postId: Int) {
+        viewModelScope.launch {
+            try {
+                _isLoading.value = true
+                val post = blogRepository.getPostDetails(postId)
+                val user = blogRepository.getUserDetails(post.userId)
+                _postDetail.value = post
+                _userDetail.value = user
+                _isLoading.value = false
+            } catch (e: Exception) {
+                _isLoading.value = false
+                _postDetail.value = null
+                _userDetail.value = null
+            }
         }
     }
 }
