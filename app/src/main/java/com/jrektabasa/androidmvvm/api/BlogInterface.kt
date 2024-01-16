0a4d08eec7eb6f7556f84c96c646b64ea82fa363
Jayrek <jrektabasa@gmail.com>
2024-01-16 11:14:14 +0800

    add page, and page limit

--- app/src/main/java/com/jrektabasa/androidmvvm/api/BlogInterface.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/api/BlogInterface.kt
@@ -4,11 +4,15 @@ import com.jrektabasa.androidmvvm.model.Post
 import com.jrektabasa.androidmvvm.model.User
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BlogInterface {
 
     @GET("posts")
-    suspend fun getPosts(): List<Post>
+    suspend fun getPosts(
+        @Query("_page") page: Int,
+        @Query("_limit") limit: Int = 10
+    ): List<Post>
 
     @GET("posts/{id}")
     suspend fun getPostDetails(@Path("id") postId: Int): Post
