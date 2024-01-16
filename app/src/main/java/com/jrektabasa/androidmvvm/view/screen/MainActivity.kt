ca67486e7662eacc2160d110f4f95b4008bf7f43
Jayrek <jrektabasa@gmail.com>
2024-01-16 18:06:42 +0800

    refactor MainActivity.kt to implement jetpack navigation

--- app/src/main/java/com/jrektabasa/androidmvvm/view/screen/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/view/screen/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.jrektabasa.androidmvvm.view.screen
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.jrektabasa.androidmvvm.R
 import com.jrektabasa.androidmvvm.databinding.ActivityMainBinding
-import com.jrektabasa.androidmvvm.model.Post
-import com.jrektabasa.androidmvvm.util.constant.IntentKeys.EXTRA_POST_ID
-import com.jrektabasa.androidmvvm.view.adapter.BlogPostAdapter
-import com.jrektabasa.androidmvvm.viewmodel.PostsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -20,58 +17,28 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var blogPostAdapter: BlogPostAdapter
-    private lateinit var linearLayoutManager: LinearLayoutManager
 
-    private val viewModel: PostsViewModel by viewModels()
-    private val blogPosts = mutableListOf<Post>()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        linearLayoutManager = LinearLayoutManager(this)
-        binding.recyclerView.layoutManager = linearLayoutManager
-        displayPosts()
-        viewModel.getPosts()
+        setSupportActionBar(binding.toolbar)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_dashboard) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfig =
+            AppBarConfiguration(setOf(R.id.postFragment, R.id.albumFragment, R.id.todoFragment))
+        setupActionBarWithNavController(navController, appBarConfig)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    private fun displayPosts() {
-        viewModel.isLoading.observe(this) { isLoading ->
-            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
-        }
-        viewModel.posts.observe(this) { posts ->
-            blogPosts.clear()
-            blogPosts.addAll(posts)
-            blogPostAdapter.notifyDataSetChanged()
-        }
-        blogPostAdapter =
-            BlogPostAdapter(this, blogPosts, object : BlogPostAdapter.OnItemClickListener {
-                override fun onClick(post: Post) {
-                    val intent = Intent(this@MainActivity, PostDetailsActivity::class.java)
-                    intent.putExtra(EXTRA_POST_ID, post.id)
-                    startActivity(intent)
-                }
-            })
-
-        binding.recyclerView.adapter = blogPostAdapter
-
-        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                val visibleItemCount: Int = linearLayoutManager.childCount
-                val totalItemCount: Int = linearLayoutManager.itemCount
-                val firstVisibleItemPosition: Int =
-                    linearLayoutManager.findFirstVisibleItemPosition()
-
-                if (viewModel.isLoading.value == false
-                    && (visibleItemCount + firstVisibleItemPosition) >= totalItemCount
-                    && firstVisibleItemPosition >= 0
-                ) {
-                    viewModel.getPosts()
-                }
-            }
-        })
-    }
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp() || super.onSupportNavigateUp()
 }
