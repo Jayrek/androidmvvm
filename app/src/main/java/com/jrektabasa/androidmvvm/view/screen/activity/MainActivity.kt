de4bae909329c006e3a342cb16ad1946703ab7f7
Jayrek <jrektabasa@gmail.com>
2024-01-17 10:37:27 +0800

    remove unused import directive

--- app/src/main/java/com/jrektabasa/androidmvvm/view/screen/activity/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/view/screen/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jrektabasa.androidmvvm.view.screen.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
