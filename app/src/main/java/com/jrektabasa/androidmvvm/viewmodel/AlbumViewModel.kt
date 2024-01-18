b367ae30dd66e9ac31e1edc620473c050c284dc7
Jayrek <jrektabasa@gmail.com>
2024-01-18 15:38:54 +0800

    update AlbumViewModel paging and observing

--- app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/AlbumViewModel.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/AlbumViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jrektabasa.androidmvvm.model.Album
+import com.jrektabasa.androidmvvm.model.UserAlbum
 import com.jrektabasa.androidmvvm.repository.BlogRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -17,22 +17,45 @@ class AlbumViewModel @Inject constructor(
     private val blogRepository: BlogRepository
 ) : ViewModel() {
 
-    private val _albums: MutableLiveData<List<Album>> = MutableLiveData(emptyList())
-    val albums: LiveData<List<Album>> = _albums
+    private val _userAlbums: MutableLiveData<List<UserAlbum>> = MutableLiveData(emptyList())
+    val userAlbums: LiveData<List<UserAlbum>> = _userAlbums
 
     private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
     val isLoading: LiveData<Boolean> = _isLoading
 
     private var currentPage = 1
 
+    private var userAlbumList = mutableListOf<UserAlbum>()
+
     fun getAlbums() {
         viewModelScope.launch {
             try {
                 _isLoading.value = true
                 val albumResponse = blogRepository.getAlbums(page = currentPage)
+
+                if (albumResponse.isEmpty()) return@launch
+
+                val usersResponse = blogRepository.getUsers()
+                val blogAlbums = _userAlbums.value ?: emptyList()
+                _userAlbums.value = blogAlbums + userAlbumList
+
+                for (album in albumResponse) {
+                    for (user in usersResponse) {
+                        if (album.userId == user.id) {
+                            userAlbumList.add(
+                                UserAlbum(
+                                    userId = album.userId,
+                                    id = album.id,
+                                    title = album.title,
+                                    userName = user.name
+                                )
+                            )
+                            _userAlbums.value = userAlbumList
+                        }
+                    }
+                }
                 currentPage += 1
-                val blogAlbums = _albums.value ?: emptyList()
-                _albums.value = blogAlbums + albumResponse
+
             } catch (e: Exception) {
                 Log.e("TAG", "getAlbums: ${e.message}")
             } finally {
