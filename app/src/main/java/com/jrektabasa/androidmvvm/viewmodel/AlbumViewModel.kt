dd0818b5446739e8b387dd497df13bbf7de87e0b
Jayrek <jrektabasa@gmail.com>
2024-01-18 13:35:24 +0800

    implement paging AlbumViewModel

--- app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/AlbumViewModel.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/viewmodel/AlbumViewModel.kt
@@ -20,15 +20,23 @@ class AlbumViewModel @Inject constructor(
     private val _albums: MutableLiveData<List<Album>> = MutableLiveData(emptyList())
     val albums: LiveData<List<Album>> = _albums
 
+    private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private var currentPage = 1
+
     fun getAlbums() {
         viewModelScope.launch {
             try {
-                val albumResponse = blogRepository.getAlbums()
-                _albums.value = albumResponse
+                _isLoading.value = true
+                val albumResponse = blogRepository.getAlbums(page = currentPage)
+                currentPage += 1
+                val blogAlbums = _albums.value ?: emptyList()
+                _albums.value = blogAlbums + albumResponse
             } catch (e: Exception) {
                 Log.e("TAG", "getAlbums: ${e.message}")
             } finally {
-
+                _isLoading.value = false
             }
         }
     }
