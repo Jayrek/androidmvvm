be4444c66ce3573aff6f30500cf87091d657fe0c
Jayrek <jrektabasa@gmail.com>
2024-01-18 13:26:35 +0800

    update AlbumFragment implement fetching of album

--- app/src/main/java/com/jrektabasa/androidmvvm/view/screen/fragment/AlbumFragment.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/view/screen/fragment/AlbumFragment.kt
@@ -1,21 +1,49 @@
 package com.jrektabasa.androidmvvm.view.screen.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jrektabasa.androidmvvm.R
+import androidx.fragment.app.viewModels
+import com.jrektabasa.androidmvvm.databinding.FragmentAlbumBinding
+import com.jrektabasa.androidmvvm.model.Album
+import com.jrektabasa.androidmvvm.view.adapter.AlbumAdapter
+import com.jrektabasa.androidmvvm.viewmodel.AlbumViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AlbumFragment : Fragment() {
+
+    private lateinit var binding: FragmentAlbumBinding
+    private lateinit var albumAdapter: AlbumAdapter
+
+    private val viewModel: AlbumViewModel by viewModels()
+    private val albumList = mutableListOf<Album>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album, container, false)
+    ): View {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getAlbums()
+
+        albumAdapter = AlbumAdapter(albumList)
+
+        viewModel.albums.observe(requireActivity()) { albums ->
+            albumList.clear()
+            albumList.addAll(albums)
+            albumAdapter.notifyDataSetChanged()
+        }
+
+        binding.recyclerView.adapter = albumAdapter
     }
 
 }
