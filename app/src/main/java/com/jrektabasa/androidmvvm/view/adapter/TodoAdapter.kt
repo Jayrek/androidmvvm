0b37b481c12bf5053092a3a71ae02e4fe38831f9
Jayrek <jrektabasa@gmail.com>
2024-01-18 12:11:28 +0800

    add todo adapter

---
+++ app/src/main/java/com/jrektabasa/androidmvvm/view/adapter/TodoAdapter.kt
@@ -0,0 +1,25 @@
+package com.jrektabasa.androidmvvm.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jrektabasa.androidmvvm.databinding.TodoItemBinding
+import com.jrektabasa.androidmvvm.model.Todo
+
+class TodoAdapter(private val todos: MutableList<Todo>) :
+    RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoAdapter.ViewHolder {
+        val view = TodoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    inner class ViewHolder(var itemBinding: TodoItemBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+
+    override fun onBindViewHolder(holder: TodoAdapter.ViewHolder, position: Int) {
+        val todo = todos[position]
+    }
+
+    override fun getItemCount(): Int = todos.size
+}
