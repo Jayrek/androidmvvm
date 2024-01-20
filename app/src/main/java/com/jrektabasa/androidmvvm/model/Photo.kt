5dde63735172f13a8eb9ac499bd37908265e939c
Jayrek <jrektabasa@gmail.com>
2024-01-20 14:43:53 +0800

    add Photo data class

---
+++ app/src/main/java/com/jrektabasa/androidmvvm/model/Photo.kt
@@ -0,0 +1,9 @@
+package com.jrektabasa.androidmvvm.model
+
+data class Photo(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String,
+)
