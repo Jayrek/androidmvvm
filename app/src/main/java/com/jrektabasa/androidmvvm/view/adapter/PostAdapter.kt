1f81b929f9706f6743b3d6888b75c75981238a31
Jayrek <jrektabasa@gmail.com>
2024-01-20 14:46:27 +0800

    refactor photo item binding

--- app/src/main/java/com/jrektabasa/androidmvvm/view/adapter/PostAdapter.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/view/adapter/PostAdapter.kt
@@ -3,7 +3,7 @@ package com.jrektabasa.androidmvvm.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.jrektabasa.androidmvvm.databinding.BlogItemBinding
+import com.jrektabasa.androidmvvm.databinding.PostItemBinding
 import com.jrektabasa.androidmvvm.model.Post
 
 class PostAdapter(
@@ -12,11 +12,11 @@ class PostAdapter(
 ) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = BlogItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = PostItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(view)
     }
 
-    inner class ViewHolder(var itemBinding: BlogItemBinding) :
+    inner class ViewHolder(var itemBinding: PostItemBinding) :
         RecyclerView.ViewHolder(itemBinding.root)
 
     override fun getItemCount(): Int = posts.size
