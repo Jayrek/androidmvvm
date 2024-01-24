5c6ea881e835e2f3e31e67dcadffef969d371241
Jayrek <jrektabasa@gmail.com>
2024-01-24 18:51:12 +0800

    add new string resource `by`

--- app/src/main/java/com/jrektabasa/androidmvvm/view/adapter/AlbumAdapter.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/view/adapter/AlbumAdapter.kt
@@ -1,12 +1,15 @@
 package com.jrektabasa.androidmvvm.view.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.jrektabasa.androidmvvm.R
 import com.jrektabasa.androidmvvm.databinding.AlbumItemBinding
 import com.jrektabasa.androidmvvm.model.UserAlbum
 
 class AlbumAdapter(
+    private val context: Context,
     private val albums: MutableList<UserAlbum>,
     private val onItemListener: OnItemListener,
 ) :
@@ -23,7 +26,7 @@ class AlbumAdapter(
         val album = albums[position]
 
         holder.itemBinding.textviewTitle.text = album.title
-        holder.itemBinding.textviewUserName.text = "by: ${album.userName}"
+        holder.itemBinding.textviewUserName.text = context.getString(R.string.by, album.userName)
         holder.itemBinding.cardView.setOnClickListener {
             onItemListener.onTap(position)
         }
