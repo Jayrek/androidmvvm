f5920e2600ea8717f4b5eb3830f5d796f89918bb
Jayrek <jrektabasa@gmail.com>
2024-01-24 18:52:55 +0800

    add new line per methods in repository

--- app/src/main/java/com/jrektabasa/androidmvvm/repository/BlogRepository.kt
+++ app/src/main/java/com/jrektabasa/androidmvvm/repository/BlogRepository.kt
@@ -10,12 +10,19 @@ import com.jrektabasa.androidmvvm.model.User
 interface BlogRepository {
 
     suspend fun getPosts(page: Int): List<Post>
+
     suspend fun getPostDetails(postId: Int): Post
+
     suspend fun getUserDetails(userId: Int): User
+
     suspend fun getUsers(): List<User>
+
     suspend fun getPostComments(postId: Int): List<Comment>
+
     suspend fun getTodos(userId: Int): List<Todo>
+
     suspend fun getAlbums(page: Int): List<Album>
+
     suspend fun getPhoto(
         albumId: Int,
         page: Int,
